346826e7d6644f4d96970ec1fea10c520437bf6e
unknown <nahuelcittadino@gmail.com>
2023-07-10 17:21:40 -0300

    first commit

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+interface FromProps {}
+
+function From({}: FormProps) {
+    const [amount, setAmount] = React.useState(1);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>)
+}
